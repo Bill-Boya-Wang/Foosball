b5ce29319c43d970ad2bf631ed53e73b924d96f6
Bill-Boya-Wang <boya.wang@gmail.com>
2023-03-22 10:11:14 -0400

    Added a panel

---
+++ Game.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game {
+    JFrame frame;
+    JPanel panel;
+
+
+    JButton changeView, levelUp, levelDown;
+
+    public Game() {
+        frame = new JFrame("map tester");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new Panel();
+        panel.setPreferredSize(new Dimension(1000, 750));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        frame.setContentPane(panel);
+
+        levelDown = new JButton("Level Down");
+        levelUp = new JButton("Level Up");
+        panel.setLayout(null);
+        panel.add(levelUp);
+        panel.add(levelDown);
+
+        levelUp.setBounds(300, 715, 100, 30);
+        levelDown.setBounds(450, 715, 100, 30);
+        levelDown.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        levelUp.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+    }
+    //rip
+    class Panel extends JPanel{
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+        }
+    }
+
+    public static void main(String[] args){
+        new Game();
+    }
+}
