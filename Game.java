771921bde6d625287fd4a11e053232f3d77b4566
Enoch <enoch1165@gmail.com>
2023-03-28 11:38:29 -0400

    Transferred code to Game paintComponent

--- Game.java
+++ Game.java
@@ -1,8 +1,11 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 
 public class Game {
@@ -13,15 +16,14 @@ public class Game {
 
 //    JButton changeView, levelUp, levelDown;
 
-    public Game() {
+    public Game() throws IOException {
         frame = new JFrame("map tester");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel = new Panel();
-        panel.setPreferredSize(new Dimension(1000, 750));
+        panel = new Panel("GoodBoard.png");
+        panel.setPreferredSize(new Dimension(1200, 650));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         frame.setContentPane(panel);
         panel.setLayout(null);
-
         ball = new Ball(t);
 
         frame.pack();
@@ -31,15 +33,21 @@ public class Game {
     }
     //rip
     class Panel extends JPanel{
+        private Image backgroundImage;
+        public Panel(String fileName) throws IOException {
+            backgroundImage = ImageIO.read(new File("GoodBoard.png"));
+        }
+
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             ball.drawBall(g, this);
+            g.drawImage(backgroundImage,0,0,1200,650,0,0,101,60,this);
             System.out.println("WHAT IS GOING ON");
             g.dispose();
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         new Game();
     }
 }
