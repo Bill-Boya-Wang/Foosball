ccde4a4e322c165fc3fae2e634ff1af07bf7f355
Enoch <enoch1165@gmail.com>
2023-05-14 14:00:17 -0400

    main manu updated

--- MainMenu.java
+++ MainMenu.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -6,19 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.text.DefaultFormatterFactory;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.time.LocalDateTime;
-import java.io.BufferedReader;
 import java.io.IOException; // Import the IOException class to handle errors
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.time.format.DateTimeFormatter;
-import java.util.Timer;
 import java.awt.Image;
 import javax.swing.*;
 public class MainMenu extends JPanel implements ActionListener {
@@ -38,20 +25,16 @@ public class MainMenu extends JPanel implements ActionListener {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
-        title = new JLabel();
-        title = new JLabel(LABEL_TEXT);
         ImageIcon startButton = new ImageIcon("NewstartButton.png");
-        ImageIcon background = new ImageIcon("newGREEN.png");
+        ImageIcon background = new ImageIcon("upgrade.png");
         JLabel label = new JLabel(background);
         start = new JButton("", startButton);
         start.setBounds(235, 500, 320, 120);
         start.setActionCommand("Start");
         start.addActionListener(this);
         label.add(start);
-        frame.add(title);
         frame.add(label);
         frame.setVisible(true);
-
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Start")) {
